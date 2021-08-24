e74843c578b493b1d681e54b9392c3c755b9ea0a
Maria Molina <hurricanemolina@gmail.com>
2021-08-24 16:18:13 -0600

    custom torch datasets

--- ML-for-S2S/torch_customdataset.py
+++ ML-for-S2S/torch_customdataset.py
@@ -30,3 +30,33 @@ class CustomDataset(Dataset):
             label = self.target_transform(label)
             
         return {'train': image, 'test': label}
+    
+    
+class CustomLSTMDataset(Dataset):
+    
+    def __init__(self, traindata, testdata, transform=None, target_transform=None):
+        
+        """Minimal dataset preprocessing for training."""
+        
+        self.img_train = traindata
+        self.img_label = testdata
+        
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        
+        return len(self.img_label)
+
+    def __getitem__(self, idx):
+        
+        image = self.img_train[:, idx]
+        label = self.img_label[:, idx]
+        
+        if self.transform:
+            image = self.transform(image)
+            
+        if self.target_transform:
+            label = self.target_transform(label)
+            
+        return {'train': image, 'test': label}
